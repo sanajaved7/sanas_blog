0c1ae6685e1226622bb2f8508aef2582389b2758
Sana Javed <sanajaved7@gmail.com>
2015-04-14 22:54:00 -0400

    Abstracted form function to dry up code, ensured we can add more than two tags, created functionality for write-in tags

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,6 @@ class Post(models.Model):
     published_date = models.DateTimeField(
         blank=True, null=True)
 
-
     def publish(self):
         self.published_date = timezone.now()
         self.save()
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import Post, Tag
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+
+
+def clean_new_tags(new_tags):
+    ''' This function takes the tags that are written and
+    adds them to the list of existing list of tags '''
+    new_tags = new_tags.split(",")
+
+    new_tag_list = []
+
+    for word in new_tags:
+        tag, created = Tag.objects.get_or_create(word=word.strip())
+        tag.save()
+        new_tag_list.append(tag)
+    return new_tag_list
+
+def process_post(cleaned_form, request):
+    ''' This function passes in the cleaned form data and creates a post '''
+    post = Post(title=cleaned_form["title"], text=cleaned_form["text"])
+    post.author = request.user
+    post.save()
+    tag_list = clean_new_tags(cleaned_form['new_tags'])
+    tag_list.extend(cleaned_form['tag'])
+    post.tags = tag_list
+    return post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -19,10 +43,7 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            form = form.cleaned_data
-            post = Post(title=form["title"], text=form["text"])
-            post.author = request.user
-            post.save()
+            post = process_post(form.cleaned_data, request)
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm()
@@ -51,9 +72,7 @@ def post_edit(request, pk):
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
+            post = process_post(form.cleaned_data, request)
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
