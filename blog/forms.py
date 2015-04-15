6c4ce387bb7efc5f668049c797a8292d8a8ca253
Sana Javed <sanajaved7@gmail.com>
2015-04-15 00:10:51 -0400

    Changed form requirements

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ from .models import Post, Tag
 class PostForm(forms.Form):
     title = forms.CharField(label="Title", max_length=200)
     text = forms.CharField(widget=forms.Textarea, label="Blog Text")
-    tag = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), label="Tags")
+    tag = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), label="Tags",required=False)
     new_tags = forms.CharField(label="New Tag", max_length=100, required=False)
 
     #class Meta:
