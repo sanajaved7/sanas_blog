7f9d161f31390584c490b0811996b07d5bcb01b6
Sana Javed <sanajaved7@gmail.com>
2015-04-16 22:48:31 -0400

    Added first test for model Post

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase, RequestFactory
+from .models import Post, Tag
+import datetime
 
-# Create your tests here.
+
+class PostTest(TestCase):
+    ''' Testing Post model '''
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username = 'sana',
+            email='sjaved@umd.edu',
+            password='password')
+
+    def test_save(self):
+        '''Tests simple save case'''
+        test_post = Post(
+            author=self.user,
+            title="Test Post",
+            text="This is a test post.",
+            created_date = datetime.date.today(),
+            published_date = datetime.date.today()
+            )
+        test_post.save()
