a5795e52b729cf8301b3341956400205d6bf5f27
Sana Javed <sanajaved7@gmail.com>
2015-04-16 23:39:36 -0400

    Added test for publish

--- blog/tests.py
+++ blog/tests.py
@@ -12,14 +12,29 @@ class PostTest(TestCase):
             username = 'sana',
             email='sjaved@umd.edu',
             password='password')
-
-    def test_save(self):
-        '''Tests simple save case'''
-        test_post = Post(
+        self.test_post = Post(
             author=self.user,
             title="Test Post",
             text="This is a test post.",
-            created_date = datetime.date.today(),
-            published_date = datetime.date.today()
+            created_date = datetime.datetime(2014,12,27)
             )
-        test_post.save()
+        self.test_post.save()
+
+    def test_retrieval(self):
+        '''Tests whether we can retrieve a saved post'''
+        retrieved_post = Post.objects.get(title="Test Post")
+        self.assertEqual(retrieved_post.author.username, "sana")
+        self.assertEqual(retrieved_post.author.email, "sjaved@umd.edu")
+        self.assertEqual(retrieved_post.text, "This is a test post.")
+        self.assertEqual(retrieved_post.created_date,
+            datetime.datetime(2014,12,27))
+
+    def test_publish(self):
+        ''' Test simple publish case '''
+        self.assertEqual(self.test_post.published_date, None)
+        self.test_post.publish()
+        self.assertNotEqual(self.test_post.published_date, None)
+
+
+
+
